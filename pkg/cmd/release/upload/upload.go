a8555030d76b1d0c8ba0fc4415f7a766d26ed5a6
adrianorozcojimenezacostagamez <87342500+hashtagmarsrover@users.noreply.github.com>
2021-12-27 16:40:01 -0800

    Initialize release-cmd

---
+++ pkg/cmd/release/upload/upload.go
@@ -0,0 +1,108 @@
+package upload
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/cli/cli/internal/ghrepo"
+	"github.com/cli/cli/pkg/cmd/release/shared"
+	"github.com/cli/cli/pkg/cmdutil"
+	"github.com/cli/cli/pkg/iostreams"
+	"github.com/spf13/cobra"
+)
+
+type UploadOptions struct {
+	HttpClient func() (*http.Client, error)
+	IO         *iostreams.IOStreams
+	BaseRepo   func() (ghrepo.Interface, error)
+
+	TagName string
+	Assets  []*shared.AssetForUpload
+
+	// maximum number of simultaneous uploads
+	Concurrency       int
+	OverwriteExisting bool
+}
+
+func NewCmdUpload(f *cmdutil.Factory, runF func(*UploadOptions) error) *cobra.Command {
+	opts := &UploadOptions{
+		IO:         f.IOStreams,
+		HttpClient: f.HttpClient,
+	}
+
+	cmd := &cobra.Command{
+		Use:   "upload <tag> <files>...",
+		Short: "Upload assets to a release",
+		Args:  cobra.MinimumNArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// support `-R, --repo` override
+			opts.BaseRepo = f.BaseRepo
+
+			opts.TagName = args[0]
+
+			var err error
+			opts.Assets, err = shared.AssetsFromArgs(args[1:])
+			if err != nil {
+				return err
+			}
+
+			opts.Concurrency = 5
+
+			if runF != nil {
+				return runF(opts)
+			}
+			return uploadRun(opts)
+		},
+	}
+
+	cmd.Flags().BoolVar(&opts.OverwriteExisting, "clobber", false, "Overwrite existing assets of the same name")
+
+	return cmd
+}
+
+func uploadRun(opts *UploadOptions) error {
+	httpClient, err := opts.HttpClient()
+	if err != nil {
+		return err
+	}
+
+	baseRepo, err := opts.BaseRepo()
+	if err != nil {
+		return err
+	}
+
+	release, err := shared.FetchRelease(httpClient, baseRepo, opts.TagName)
+	if err != nil {
+		return err
+	}
+
+	uploadURL := release.UploadURL
+	if idx := strings.IndexRune(uploadURL, '{'); idx > 0 {
+		uploadURL = uploadURL[:idx]
+	}
+
+	var existingNames []string
+	for _, a := range opts.Assets {
+		for _, ea := range release.Assets {
+			if ea.Name == a.Name {
+				a.ExistingURL = ea.APIURL
+				existingNames = append(existingNames, ea.Name)
+				break
+			}
+		}
+	}
+
+	if len(existingNames) > 0 && !opts.OverwriteExisting {
+		return fmt.Errorf("asset under the same name already exists: %v", existingNames)
+	}
+
+	opts.IO.StartProgressIndicator()
+	err = shared.ConcurrentUpload(httpClient, uploadURL, opts.Concurrency, opts.Assets)
+	opts.IO.StopProgressIndicator()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/iostreams/iostreams.go
@@ -0,0 +1,183 @@
+package iostreams
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+type IOStreams struct {
+	In     io.ReadCloser
+	Out    io.Writer
+	ErrOut io.Writer
+
+	colorEnabled bool
+
+	progressIndicatorEnabled bool
+	progressIndicator        *spinner.Spinner
+
+	stdinTTYOverride  bool
+	stdinIsTTY        bool
+	stdoutTTYOverride bool
+	stdoutIsTTY       bool
+	stderrTTYOverride bool
+	stderrIsTTY       bool
+}
+
+func (s *IOStreams) ColorEnabled() bool {
+	return s.colorEnabled
+}
+
+func (s *IOStreams) SetStdinTTY(isTTY bool) {
+	s.stdinTTYOverride = true
+	s.stdinIsTTY = isTTY
+}
+
+func (s *IOStreams) IsStdinTTY() bool {
+	if s.stdinTTYOverride {
+		return s.stdinIsTTY
+	}
+	if stdin, ok := s.In.(*os.File); ok {
+		return isTerminal(stdin)
+	}
+	return false
+}
+
+func (s *IOStreams) SetStdoutTTY(isTTY bool) {
+	s.stdoutTTYOverride = true
+	s.stdoutIsTTY = isTTY
+}
+
+func (s *IOStreams) IsStdoutTTY() bool {
+	if s.stdoutTTYOverride {
+		return s.stdoutIsTTY
+	}
+	if stdout, ok := s.Out.(*os.File); ok {
+		return isTerminal(stdout)
+	}
+	return false
+}
+
+func (s *IOStreams) SetStderrTTY(isTTY bool) {
+	s.stderrTTYOverride = true
+	s.stderrIsTTY = isTTY
+}
+
+func (s *IOStreams) IsStderrTTY() bool {
+	if s.stderrTTYOverride {
+		return s.stderrIsTTY
+	}
+	if stderr, ok := s.ErrOut.(*os.File); ok {
+		return isTerminal(stderr)
+	}
+	return false
+}
+
+func (s *IOStreams) CanPrompt() bool {
+	return s.IsStdinTTY() && s.IsStdoutTTY()
+}
+
+func (s *IOStreams) StartProgressIndicator() {
+	if !s.progressIndicatorEnabled {
+		return
+	}
+	sp := spinner.New(spinner.CharSets[11], 400*time.Millisecond, spinner.WithWriter(s.ErrOut))
+	sp.Start()
+	s.progressIndicator = sp
+}
+
+func (s *IOStreams) StopProgressIndicator() {
+	if s.progressIndicator == nil {
+		return
+	}
+	s.progressIndicator.Stop()
+	s.progressIndicator = nil
+}
+
+func (s *IOStreams) TerminalWidth() int {
+	defaultWidth := 80
+	if s.stdoutTTYOverride {
+		return defaultWidth
+	}
+
+	if w, _, err := terminalSize(s.Out); err == nil {
+		return w
+	}
+
+	if isCygwinTerminal(s.Out) {
+		tputCmd := exec.Command("tput", "cols")
+		tputCmd.Stdin = os.Stdin
+		if out, err := tputCmd.Output(); err == nil {
+			if w, err := strconv.Atoi(strings.TrimSpace(string(out))); err == nil {
+				return w
+			}
+		}
+	}
+
+	return defaultWidth
+}
+
+func (s *IOStreams) ColorScheme() *ColorScheme {
+	return NewColorScheme(s.ColorEnabled())
+}
+
+func System() *IOStreams {
+	stdoutIsTTY := isTerminal(os.Stdout)
+	stderrIsTTY := isTerminal(os.Stderr)
+
+	io := &IOStreams{
+		In:           os.Stdin,
+		Out:          colorable.NewColorable(os.Stdout),
+		ErrOut:       colorable.NewColorable(os.Stderr),
+		colorEnabled: os.Getenv("NO_COLOR") == "" && stdoutIsTTY,
+	}
+
+	if stdoutIsTTY && stderrIsTTY {
+		io.progressIndicatorEnabled = true
+	}
+
+	// prevent duplicate isTerminal queries now that we know the answer
+	io.SetStdoutTTY(stdoutIsTTY)
+	io.SetStderrTTY(stderrIsTTY)
+	return io
+}
+
+func Test() (*IOStreams, *bytes.Buffer, *bytes.Buffer, *bytes.Buffer) {
+	in := &bytes.Buffer{}
+	out := &bytes.Buffer{}
+	errOut := &bytes.Buffer{}
+	return &IOStreams{
+		In:     ioutil.NopCloser(in),
+		Out:    out,
+		ErrOut: errOut,
+	}, in, out, errOut
+}
+
+func isTerminal(f *os.File) bool {
+	return isatty.IsTerminal(f.Fd()) || isatty.IsCygwinTerminal(f.Fd())
+}
+
+func isCygwinTerminal(w io.Writer) bool {
+	if f, isFile := w.(*os.File); isFile {
+		return isatty.IsCygwinTerminal(f.Fd())
+	}
+	return false
+}
+
+func terminalSize(w io.Writer) (int, int, error) {
+	if f, isFile := w.(*os.File); isFile {
+		return terminal.GetSize(int(f.Fd()))
+	}
+	return 0, 0, fmt.Errorf("%v is not a file", w)
+}
